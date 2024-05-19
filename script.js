0df6006ccf84e5f856167abad2e40ba392264cd0
pariitosh <paritoshsagar25@gmail.com>
2024-05-19 23:18:19 +0530

    done

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'yellow';
+ctx.fillStyle = 'black';
 
-function drawRect(x, y) {
+let shapes=[{x:10,y:10,width:100,height:100,isMoving:false},{x:100,y:200,width:100,height:100,isMoving:false}]
+
+function drawRect() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(x-150, y, 150, 100);
+
+    for (let i in shapes){
+        console.log(shapes[i])
+        ctx.strokeRect (shapes[i].x,shapes[i].y,shapes[i].width,shapes[i].height)
+    }
+    
     ctx.beginPath();
-    ctx.moveTo(500,250);
-    ctx.lineTo(x, y);
-    ctx.stroke();    
+    ctx.moveTo(shapes[0].x+50, shapes[0].y+50); 
+    ctx.lineTo(shapes[1].x+50, shapes[1].y+50);
+    ctx.stroke();
 }
 
 
-drawRect(200, 200);
+drawRect();
 
 let mousedown=false
 
 canvas.addEventListener('mousemove', function(event) {
-    if(mousedown){
+   
         var rect = canvas.getBoundingClientRect();
         var x = event.clientX - rect.left;
         var y = event.clientY - rect.top;
-        drawRect(x, y);
-    }
-    console.log(mousedown)
+        for (let j in shapes){
+            if(shapes[j].isMoving===true){
+                shapes[j].x=x
+                shapes[j].y=y
+                drawRect()
+            }
+            
+
+            
+        }
+
+        
+    
+    
 });
 
 canvas.addEventListener('mousedown',(event) => {
+
     mousedown=true
-    console.log('1s')
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    for (let j in shapes){
+        if(x >= shapes[j].x && x <= shapes[j].x + shapes[j].width && y >= shapes[j].y && y <= shapes[j].y + shapes[j].height){
+            console.log('inside')
+            shapes[j].isMoving=true;
+        }
+        
+    }
 })
 canvas.addEventListener('mouseup',() => {
     mousedown=false
+    for(let k in shapes){
+        shapes[k].isMoving=false
+    }
 })
